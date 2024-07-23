6f9074aca553d8d87677a9436c80df91cee8ebe0
heeeione <heewon410@naver.com>
2024-07-23 17:24:17 +0900

    fix: dotenv error

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const app = express();
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/sms', smsRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
